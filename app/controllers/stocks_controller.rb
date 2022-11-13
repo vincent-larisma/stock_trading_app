d84f32859d313f347e9e8f726a36070fd5de18b4
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-11-13 19:36:33 +0800

    if applied, this commit will use api to add user's stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,7 @@
 class StocksController < ApplicationController
     # before_action :authenticate_user!
+    before_action :initialize_stock, only: [:create]
+
     def index
         @stocks = Stock.all
     end
@@ -9,18 +11,34 @@ class StocksController < ApplicationController
     end
 
     def create
-        @stock = Stock.find_or_initialize_by(symbol: stock_params[:symbol])
-        @stock.shares += stock_params[:shares].to_i
+        begin
+            
+            @quote = @client.quote(params[:symbol])
+            @stock = Stock.find_or_initialize_by(symbol: params[:symbol])
 
-        if @stock.update(stock_params.except(:shares))
-            @stock.transactions.create(action_type: 'buy', company_name: @stock.company_name, shares: @stock.shares, cost_price: @stock.cost_price)
-            redirect_to root_path
-        else
-            render :new
+            #manually add params
+            @stock.shares += stock_params[:shares].to_i
+            @stock.symbol = @quote.symbol
+            @stock.company_name = @quote.company_name 
+            @stock.cost_price = @quote.latest_price
+
+            if @stock.save
+                @stock.transactions.create(action_type: 'buy', company_name: @quote.company_name, shares: @stock.shares, cost_price: @quote.latest_price)
+                redirect_to root_path
+            else
+                redirect_to find_stock_path
+            end
+
+        rescue IEX::Errors::SymbolNotFoundError
+            flash[:error] = "Sorry, the symbol is not valid."
+            redirect_to find_stock_path
         end
     end
 
     private
+    def initialize_stock
+        @client = IEX::Api::Client.new( publishable_token: 'pk_d10cc347da074a029aa3f948a7dc4e47', secret_token: 'sk_3d7f3084534a4f2fb80218e34f340b64', endpoint: 'https://cloud.iexapis.com/v1')
+    end
 
     def stock_params
         params.require(:stock).permit(:symbol, :company_name, :shares, :cost_price)
--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,24 +1,39 @@
 class TradesController < ApplicationController
+    before_action :initialize_stock, only: [:find_stock, :buy_stock]
+
     def find_stock
-        client = IEX::Api::Client.new( publishable_token: 'pk_d10cc347da074a029aa3f948a7dc4e47', secret_token: 'sk_3d7f3084534a4f2fb80218e34f340b64', endpoint: 'https://cloud.iexapis.com/v1')
-        symbols = client.ref_data_symbols
-        
-        if params[:search_stock]
-            symbols.each do |symbol|
-                if symbol.symbol == params[:search_stock]
-                    redirect_to buy_stock_path(params[:search_stock])
-                end
-            end
+        begin
+            quote = @client.quote(params[:search_stock])
+            redirect_to buy_stock_path(params[:search_stock]) # '/buy_stock/GOOGL'
+        rescue IEX::Errors::SymbolNotFoundError
+            flash[:error] = "Sorry, the symbol is not valid."
+            render :find_stock 
         end
-        
     end
 
     def sell_stock
-
+       
     end
 
     def buy_stock
+        begin
+            @quote = @client.quote(params[:symbol])
+            @stock = Stock.new
 
+        rescue IEX::Errors::SymbolNotFoundError
+            flash[:error] = "Sorry, the symbol is not valid."
+            render :find_stock 
+        end
+    end
+
+    private
+
+    def initialize_stock
+        @client = IEX::Api::Client.new( publishable_token: 'pk_d10cc347da074a029aa3f948a7dc4e47', secret_token: 'sk_3d7f3084534a4f2fb80218e34f340b64', endpoint: 'https://cloud.iexapis.com/v1')
     end
     
+    def stock_params
+        params.require(:stock).permit(:symbol, :company_name, :shares, :cost_price)
+        # params.require(:stock).permit(:shares)
+    end
 end
