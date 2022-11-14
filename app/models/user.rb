2e7bbef2386461e2dca88e3217809a81ba45d616
jbdevelop3r <justinebanogon14@gmail.com>
2022-11-14 20:01:55 +0800

    if applied, first to register will be an admin and the incoming users will be a pending trader

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,22 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  enum role: [:trader, :admin]
-  after_initialize :set_default_role_trader, :if  => :new_record?
+  if User.count == 0
+    enum role: [:trader, :admin]
+    after_initialize :set_default_role_admin, :if  => :new_record?
+    enum account_status: [:pending, :approved]
+    after_initialize :set_account_approved, :if  => :new_record?
+  end
 
-  enum account_status: [:pending, :approved]
-  after_initialize :set_account_pending, :if  => :new_record?
-
+  if User.count >= 1
+    enum role: [:trader, :admin]
+    after_initialize :set_default_role_trader, :if  => :new_record?
+    enum account_status: [:pending, :approved]
+    after_initialize :set_account_pending, :if  => :new_record?
+  end
 
   def set_default_role_trader
     self.role ||= :trader
