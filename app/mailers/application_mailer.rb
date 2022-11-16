cf2b8752886b989f54e36527a4f52b10d030f9b3
jbdevelop3r <justinebanogon14@gmail.com>
2022-11-16 19:41:02 +0800

    if applied, this commill make the app send email to the user when they buy stocks

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'scammer@stock_app.com'
   layout 'mailer'
 
-  def approve_email(user)
-    mail(:to => user.email, :subject => 'You successfully bought stocks!')       
+  def approve_email(user, stock)
+    mail(:to => user.email, :subject => "You successfully bought #{stock.shares} stocks of #{stock.symbol}!")       
   end
 end
