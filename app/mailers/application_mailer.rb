e28a5841b4b53876d22bb56d805d57ae5a2e6139
jbdevelop3r <justinebanogon14@gmail.com>
2022-11-22 13:38:33 +0800

    if applied, devise forms will have their designs

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,11 +2,11 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'scammer@stock_app.com'
   layout 'mailer'
 
-  def approve_email(user, stock)
+  def buy_stocks(user, stock)
     mail(:to => user.email, :subject => "You successfully bought #{stock.shares} stocks of #{stock.symbol}!")       
   end
 
   def approve_trader_email(user)
-    mail(:to => user.email, :subject => "Your account has been approved. You can now buy and sell stocks!")
+    mail(:to => user.email, :subject => "Your account has been approved. Re-login your account so you can start buying and selling stocks!")
   end
 end
