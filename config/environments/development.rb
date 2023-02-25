04b424843984f404568a814d272edf4378ba5576
jbdevelop3r <justinebanogon14@gmail.com>
2023-02-25 16:58:02 +0800

    added ENVs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.hosts << "stocktradingapp-production.up.railway.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
